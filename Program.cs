38708a0cb8ff3becc137f94302e739a498794863
mohammedrady44 <121281287+mohammedrady44@users.noreply.github.com>
2022-12-23 17:43:03 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,60 +1,12 @@
 ﻿using System;
 
-namespace project1
+namespace project_2
 {
     class Program
     {
         static void Main(string[] args)
         {
-
-
-            int i, j, flag;
-            Console.WriteLine("enter the first value ");
-            int x = int.Parse(Console.ReadLine());
-            Console.WriteLine("enter the last value ");
-            int y = int.Parse(Console.ReadLine());
-            Console.Write("\nPrime numbers between " + x
-                    + " and " + y + " are: ");
-
-            if (x == 1)
-            {
-                Console.Write(x + " ");
-                x++;
-                if (x >= 2)
-                {
-                    Console.Write(x + " ");
-                    x++;
-                }
-            }
-            if (x == 2)
-            {
-                Console.Write(x + " ");
-            }
-
-            if (x % 2 == 0)
-            {
-                x++;
-            }
-
-            for (i = x; i <= y; i += 2)
-            {
-
-                flag = 1;
-
-                for (j = 2; j * j <= i; ++j)
-                {
-                    if (i % j == 0)
-                    {
-                        flag = 0;
-                        break;
-                    }
-                }
-
-                if (flag == 1)
-                {
-                    Console.Write(i + " ");
-                }
-            }
+            Console.WriteLine("Hello World!");
         }
     }
 }
